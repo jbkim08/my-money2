5091b87daaf1909f81098a2e7b54217d1bddb994
JBKIM <drv98@naver.com>
2024-06-19 15:29:50 +0900

    사인업

--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -1,7 +1,48 @@
+import { useState } from "react";
 import styles from "./Signup.module.css";
 
 function Signup() {
-  return <div>가입하기</div>;
+  const [email, setEmail] = useState("");
+  const [passowrd, setPassword] = useState("");
+  const [name, setName] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email, passowrd, name);
+  };
+  return (
+    <form onSubmit={handleSubmit} className={styles["signup-form"]}>
+      <h2>Signup</h2>
+      <label>
+        <span>email:</span>
+        <input
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+
+      <label>
+        <span>passowrd:</span>
+        <input
+          type="passowrd"
+          onChange={(e) => setPassword(e.target.value)}
+          value={passowrd}
+        />
+      </label>
+
+      <label>
+        <span>name:</span>
+        <input
+          type="text"
+          onChange={(e) => setName(e.target.value)}
+          value={name}
+        />
+      </label>
+
+      <button className="btn">가입하기</button>
+    </form>
+  );
 }
 
 export default Signup;
