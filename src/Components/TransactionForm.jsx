1ec538c896f8d279eb33c00f46552ca92eb2e97e
JBKIM <drv98@naver.com>
2024-06-20 14:18:59 +0900

    파이어스토어에 입력하기

--- src/Components/TransactionForm.jsx
+++ src/Components/TransactionForm.jsx
@@ -1,16 +1,28 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useFirestore } from "../hooks/useFirestore";
 
-export default function TransactionForm() {
+export default function TransactionForm({ uid }) {
   const [name, setName] = useState("");
   const [amount, setAmount] = useState("");
+  const { addDocument, response } = useFirestore("transactions"); //트랜잭션에 데이터 저장
 
+  //클라우드 파이어스토어는 문서형 DB로 테이블이 정해지지 않고 객체형식으로 입력함.
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log({
+    addDocument({
+      uid,
       name,
       amount,
     });
   };
+
+  //db에 새문서 저장후 폼 내용 없애기
+  useEffect(() => {
+    if (response.success) {
+      setName("");
+      setAmount("");
+    }
+  }, [response.success]);
 
   return (
     <div>
